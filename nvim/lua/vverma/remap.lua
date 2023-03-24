25f0f5cb6600aa17cbfad6111761cc345b6f7b22
Vaibhav Verma <vaibhav@betteromics.com>
2023-03-24 14:52:36 -0700

    Use fd for fzf
    and another nvim keybinding

--- nvim/lua/vverma/remap.lua
+++ nvim/lua/vverma/remap.lua
@@ -24,6 +24,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "q:", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- quickfix commands
